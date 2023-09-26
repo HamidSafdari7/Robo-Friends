c07611d1eaa386d595eb3f309e7e5f42138c2524
HamidSafdari7 <93097273+HamidSafdari7@users.noreply.github.com>
2023-09-26 17:19:41 +0330

    add pwa

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -7,7 +7,8 @@ const SearchBox = (props) => {
         <>
             <div className='pa2'>
 
-                <input 
+                <input
+                    aria-label='Search Robots' 
                     className='pa3 ba b--green bg-lightest-blue'
                     type="search"
                     placeholder='Search robots'
